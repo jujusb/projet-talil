2d986dc5bd227160251d1ab1714ff8fd7b96909c
julio <julio.santilario-berthilier@insa-rennes.fr>
2020-05-03 15:22:14 +0200

    Modification du  système de neurone pour la recréation du fichier fonctionnel OK | résultat à 78% pour 20 époques

--- neuronessystem.py
+++ neuronessystem.py
@@ -4,7 +4,7 @@ from keras.optimizers import SGD,Adam
 from keras.models import Model
 from sklearn.metrics import accuracy_score,classification_report
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
+#import tensorflow as tf
 # pour l'utilisation de Raoh
 #configT = tf.ConfigProto()
 #configT.gpu_options.allow_growth = True
@@ -70,7 +70,7 @@ X=np.asarray(data)
 Y=np.asarray(prediction)
 Y=np.expand_dims(Y,2)
 print(Y.shape)
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.20,random_state=42)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.20)#,random_state=42)
 print(X_train)
 print(Y_train)
 print(X_test)
@@ -84,7 +84,7 @@ opti = SGD(lr=0.4, nesterov=False)#decay=1e−6, momentum=0.9
 #input: matrix (nbsamples∗inputsize)
 nbsamples=len(data)#4978
 MAX_SEQ_SIZE=86
-hiddensize=1
+hiddensize=2
 tailleDictionnaire=len(mots)
 nbLabels=len(motspred)
 #X:(nbexamples∗MAX_SEQ_SIZE)
@@ -98,34 +98,44 @@ out = TimeDistributed(Dense(units=nbLabels,activation='softmax'))(drop)
 
 model = Model(inputs=entree,outputs=out)
 model.compile(loss='sparse_categorical_crossentropy',optimizer=opti,metrics=['accuracy'])
-
+nbEpoques=50
 #Y_train: output (labels vector, coded as numbers [0,nblabels])
-model.fit(X_train, Y_train, epochs=20, batch_size=32)
+model.fit(X_train, Y_train, epochs=nbEpoques, batch_size=32)
 predictions = model.predict(X_test).argmax(-1)
 print(predictions)
 
 
-atisRN = open("atisRN20prediction","w")
+atisRN = open("atisRN"+str(nbEpoques)+"prediction","w")
 compt=0
 print(len(predictions))
 print(len(X_test))
 print(len(Y_test))
 for pred in predictions :
+	predreel=Y_test[compt] #prediction
+	phrase=X_test[compt] #data
 	if(compt==0):
 		print("\npred=")
 		print(pred)
-		predreel=Y_test[compt] #prediction
 		print("\npredreel=")
 		print(predreel)
-		phrase=X_test[compt] #data
 		print("\nphrase=")
 		print(phrase)
 	compt+=1
 	i=0
 	while phrase[i]!=0:
-		atisRN.write(str(mots[phrase[i]])+"\t"+str(motspred[predreel[i]]) + "\n")
+		mot=mots[phrase[i]]
+		predmotreel=motspred[predreel[i][0]]	
+		predmot=motspred[pred[i]]
+		atisRN.write(mot)
+		atisRN.write("\t")
+		atisRN.write(str(predmotreel))
+		atisRN.write("\t")
+		atisRN.write(str(predmot))
+		atisRN.write("\n")
+		#atisRN.write(mots[phrase[i]]+"\t"+motspred[predreel[i]] + "\n")
 		#t"+str(motspred[pred[i]])+"\n")
-		print(str(mots[phrase[i]]) +"\t"+str(motspred[predreel[i]])+ "\n")
+		
+		#print(mots[phrase[i]] +"\t"+motspred[predreel[i]]+ "\n")
 		#t"+ str(motspred[pred[i]])+"\n")
 		i+=1
 	atisRN.write("\n")
